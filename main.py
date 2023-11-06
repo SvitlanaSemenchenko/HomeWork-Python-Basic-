e10edacea8b251dcb0a8c835b02a7f4e202fd5d3
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-11-06 19:26:36 +0200

    HomeWork2.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# запит вводу
 x = int (input("Please enter a 4-digit whole number:  "))
-# перевірка
 a = int(x) // 1000
 x = int(x) % 1000
 b = x // 100
 x = x % 100
 c = x // 10
 d = x % 10
-# вивід
 print(a)
 print(b)
 print(c)

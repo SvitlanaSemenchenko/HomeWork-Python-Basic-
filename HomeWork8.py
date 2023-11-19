6db92515d86aee0c632f6559509491932e296350
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-11-19 23:01:11 +0200

    Update HomeWork8.py

--- HomeWork8.py
+++ HomeWork8.py
@@ -1,6 +1,6 @@
-def calculate_sum_multiply(list):
+def sum_multiply(list):
     if not list:  # Check for an empty list
         return 0
-    sum_even_indexes = sum(list[i] for i in range(0, len(list), 2))
-    result = sum_even_indexes * list[-1]
+    sum_even_i = sum(list[i] for i in range(0, len(list), 2))  # Sum elements with even indexes
+    result = sum_even_i * list[-1]  # Multiply by the last element
     return result

bd53184afc5700461e1d5e47e882bbe3e80db88b
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-11-19 22:58:37 +0200

    Update HomeWork8.py

--- HomeWork8.py
+++ HomeWork8.py
@@ -4,8 +4,3 @@ def calculate_sum_multiply(list):
     sum_even_indexes = sum(list[i] for i in range(0, len(list), 2))
     result = sum_even_indexes * list[-1]
     return result
-
-# Example:
-# my_list = [0, 1, 7, 2, 4, 8]
-# result = calculate_sum_multiply(my_list)
-# print(result)

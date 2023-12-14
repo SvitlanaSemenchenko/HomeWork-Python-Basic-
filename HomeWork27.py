af7fb25512287ca843294c13ce28a17a6265bbd3
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-12-14 22:37:56 +0200

    Update HomeWork27.py

--- HomeWork27.py
+++ HomeWork27.py
@@ -1,40 +1,41 @@
 class Item:
     def __init__(self, name, price, description, dimensions):
-        self.name = name
         self.price = price
         self.description = description
         self.dimensions = dimensions
+        self.name = name
 
     def __str__(self):
         return f"{self.name}, price: {self.price}"
 
 class User:
-    def __init__(self, first_name, last_name, phone_number):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = phone_number
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 class Purchase:
-    def __init__(self, customer):
-        self.customer = customer
-        self.items = {}
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
 
-    def add_item(self, item, quantity):
+    def add_item(self, item, cnt):
         if item in self.items:
-            self.items[item] += quantity
+            self.items[item] += cnt
         else:
-            self.items[item] = quantity
+            self.items[item] = cnt
 
     def calculate_total(self):
-        total = sum(item.price * quantity for item, quantity in self.items.items())
+        total = sum(item.price * cnt for item, cnt in self.items.items())
         return total
 
     def __str__(self):
-        order_info = f"User: {self.customer}\nItems:\n"
-        for item, quantity in self.items.items():
-            order_info += f"{item.name}: {quantity} pcs.\n"
+        order_info = f"User: {self.user}\nItems:\n"
+        for item, cnt in self.items.items():
+            order_info += f"{item.name}: {cnt} pcs.\n"
         order_info += f"Total: {self.calculate_total()}"
         return order_info

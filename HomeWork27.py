1b9fa48c47ce28bbd30bc3dbd726d9642a11b0ab
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-12-15 13:50:04 +0200

    Update HomeWork27.py

--- HomeWork27.py
+++ HomeWork27.py
@@ -15,7 +15,7 @@ class User:
         self.numberphone = numberphone
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.name} {self.surname} {self.numberphone}"
 
 class Purchase:
     def __init__(self, user):
@@ -24,18 +24,48 @@ class Purchase:
         self.total = 0
 
     def add_item(self, item, cnt):
-        if item in self.items:
-            self.items[item] += cnt
-        else:
-            self.items[item] = cnt
+        self.products[item] = cnt
 
     def calculate_total(self):
-        total = sum(item.price * cnt for item, cnt in self.items.items())
+        total = sum(item.price * cnt for item, cnt in self.products.items())
         return total
 
     def __str__(self):
         order_info = f"User: {self.user}\nItems:\n"
-        for item, cnt in self.items.items():
+        for item, cnt in self.products.items():
             order_info += f"{item.name}: {cnt} pcs.\n"
         order_info += f"Total: {self.calculate_total()}"
         return order_info
+
+lemon = Item('lemon', 5, "yellow", "small")
+apple = Item('apple', 2, "red", "middle")
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+Total: 60
+"""
+
+assert isinstance(cart.user, User) is True, 'Instance of the User class'
+assert cart.calculate_total() == 60, "Total should be 60"
+cart.add_item(apple, 10)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+Total: 40
+"""
+
+assert cart.calculate_total() == 40, "Total should be 40"

a9f2bbe6dac0f72ef3b25dcd748af5240ee03b32
SvitlanaSemenchenko <149714612+SvitlanaSemenchenko@users.noreply.github.com>
2023-12-17 14:23:17 +0200

    Create HoneWork28.py

---
+++ HoneWork28.py
@@ -0,0 +1,42 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, {self.age} years old, {self.gender}'
+
+
+class Student(Human):
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{super().__str__()}, Record Book: {self.record_book}'
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        self.group.add(student)
+
+    def delete_student(self, last_name):
+        student_to_remove = self.find_student(last_name)
+        if student_to_remove:
+            self.group.remove(student_to_remove)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number:{self.number}\n{all_students}'
